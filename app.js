da6116733c1e1eaf4f249f4e8a7e6e8388a89a38
Matheus Ferraz <pro.ferrazmatheus@gmail.com>
2024-01-05 14:48:37 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,39 @@
+var content = document.querySelector('div#app main')
+var button = document.querySelector('div#app form button')
+input = document.querySelector('div#app form input')
+function run(event) {
+    var newInput = input.value
+    newInput = newInput.replace(' ', '')
+    newInput = newInput.replace('.', '')
+    newInput = newInput.replace('-', '')
+    newInput = newInput.trim()
+    event.preventDefault()
+    console.log(newInput)
+
+    axios.get('https://viacep.com.br/ws/' + newInput + '/json/').then(function(response) {
+        var result = response.data
+        console.log(result)
+
+        if (result.erro) {
+           throw new Error('CEP Inválido')
+        }
+        content.innerHTML = ''
+        createLine(result.localidade + ' - ' + result.uf)
+        createLine(result.bairro)
+        createLine(result.logradouro  + ' N°' + result.ddd)
+
+    })
+    .catch(function(error) {
+        // console.log("Não foi possível encontrar o endereço.", error)
+        content.innerHTML = ''
+        createLine('CEP inválido')
+    }) 
+}
+function createLine(text) {
+    var line = document.createElement('p')
+    var text = document.createTextNode(text)
+    line.appendChild(text)
+    content.append(line)
+}
+
+button.addEventListener('click', run)
